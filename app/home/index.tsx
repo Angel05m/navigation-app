7448572083c28d3547808aa0341fd21c0fe730ab
Angel05m <aasm052002@gmail.com>
2026-04-01 15:38:04 -0700

    Pre-diseño botones

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'expo-router'
+import CustomButton from '@/components/shared/CustomButton'
 import React from 'react'
 import { View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -6,10 +6,14 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const Home = () => {
   return (
     <SafeAreaView>
-      <View className='px-5'>
-        <Link className='mb-5' href='/products'>Productos{''}</Link>
+      <View className='px-5 mt-5'>
+
+        <CustomButton color={'primary'}></CustomButton>
+
+
+        {/* <Link className='mb-5' href='/products'>Productos{''}</Link>
         <Link className='mb-5' href='/profile'>Perfil{''}</Link>
-        <Link className='mb-5' href='/settings'>Ajuste{''}</Link>
+        <Link className='mb-5' href='/settings'>Ajuste{''}</Link> */}
 
       </View>
     </SafeAreaView>
